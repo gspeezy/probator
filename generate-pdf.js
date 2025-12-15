cb5cd8fdd73d332c1e363bcbb65d5b81b2c2cf68
gspeezy <190349570+gspeezy@users.noreply.github.com>
2025-12-16 08:30:01 +1300

    Use Puppeteer bundled Chrome for Render

--- generate-pdf.js
+++ generate-pdf.js
@@ -36,13 +36,13 @@ async function generateProbatePDF(data) {
         }
         
         const browser = await puppeteer.launch({
-            headless: true,
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+            headless: 'new',
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',
-                '--disable-gpu'
+                '--disable-gpu',
+                '--disable-software-rasterizer'
             ]
         });
         
