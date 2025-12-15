57ec2b3f5380b028c5fc9b4f411c512a8a721200
gspeezy <190349570+gspeezy@users.noreply.github.com>
2025-12-16 08:25:13 +1300

    Update Puppeteer executable path

--- generate-pdf.js
+++ generate-pdf.js
@@ -37,13 +37,12 @@ async function generateProbatePDF(data) {
         
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--single-process',
-                '--no-zygote'
+                '--disable-gpu'
             ]
         });
         
